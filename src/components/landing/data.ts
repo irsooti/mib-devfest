b21ff6e56784a502bdf1c045fdfede6cb133058d
Daniele Irsuti <irsooti+code@gmail.com>
2025-10-14 23:33:26 +0200

    download app

---
+++ src/components/landing/data.ts
@@ -0,0 +1,11 @@
+export const numbers = {
+  speakers: 30,
+  attendees: "800+",
+  sponsors: "15+",
+};
+
+export const storeUrl = {
+  apple: "https://apps.apple.com/it/app/devfest-pescara/id6471958039",
+  googlePlay:
+    "https://play.google.com/store/apps/details?id=it.gdgpescara.filch",
+};
--- src/components/landing/numbers.ts
+++
@@ -1,5 +0,0 @@
-export const numbers = {
-  speakers: 30,
-  attendees: "800+",
-  sponsors: "15+",
-};
